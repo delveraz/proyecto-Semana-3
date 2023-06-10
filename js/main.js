771eeb6e02a91b9514b003006d3288c749e9fd4d
Delver Faniel Andrade Zamoran <delveraz14@gmail.com>
2023-06-09 20:23:00 -0600

    widget

---
+++ js/main.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function($){ 
+    $("#ul1").responsiveSlider({
+        auto: true,
+        sliderDelay: 4000,
+        easing:"easeInOutCubic",
+        rolloverMode: true,
+        buttonBar: true,
+    });
+
+    $("#ul2").responsiveSlider({
+        auto: true,
+        sliderDelay: 3000,
+        easing:"easeInOutCubic",
+        rolloverMode: true,
+        buttonBar: true,
+        buttonBarClass: "botoneraSec",
+    });
+
+
+});
